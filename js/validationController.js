da362145834b4aec26927dac6a450a9c1bc1f160
Shehanka <hcsperera@outlook.com>
2018-07-23 22:13:19 +0530

    jQuery lib

--- js/validationController.js
+++ js/validationController.js
@@ -1,12 +1,14 @@
-let TEL, EMAIL;
+/**
+ * Created by PhpStorm.
+ * User: chamodshehanka
+ * Date: 7/18/2018
+ * Time: 4:27 PM
+ */
+
+let TEL = 'tel';
+let EMAIL = 'email';
+let Name = 'name';
 
 function validate(type, data) {
-    switch (type){
-        case TEL:
-            break;
-        case EMAIL:
-            break;
-        default:
-            console.log("Invalid type!!!");
-    }
+    if (type === TEL)
 }
