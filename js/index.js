5376f165ae40fdb222750f6503e0bc36ef4befa2
Shehanka <hcsperera@outlook.com>
2018-08-10 14:07:53 +0530

    Making login action

--- js/index.js
+++ js/index.js
@@ -8,3 +8,11 @@
 let email = $('#txtEmail').val();
 let password = $('#txtPassword').val();
 
+function loginAction(email, password) {
+    if (email === 'admin@rent18.com' && password === 1234){
+        window.location.replace("../add-vehicle.html");
+    } else {
+
+    }
+}
+
