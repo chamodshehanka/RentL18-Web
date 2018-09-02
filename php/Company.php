c057b299012a3749ac66dbff15022dca29c5c21e
Shehanka <hcsperera@outlook.com>
2018-09-02 23:08:52 +0530

    Company class created

--- php/Company.php
+++ php/Company.php
@@ -15,4 +15,24 @@ class Company
     private $contact;
     private $noOfVehiclesOwn;
 
+    /**
+     * Company constructor.
+     * @param $companyID
+     * @param $companyName
+     * @param $addressLine1
+     * @param $addressLine2
+     * @param $contact
+     * @param $noOfVehiclesOwn
+     */
+    public function __construct($companyID, $companyName, $addressLine1, $addressLine2, $contact, $noOfVehiclesOwn)
+    {
+        $this->companyID = $companyID;
+        $this->companyName = $companyName;
+        $this->addressLine1 = $addressLine1;
+        $this->addressLine2 = $addressLine2;
+        $this->contact = $contact;
+        $this->noOfVehiclesOwn = $noOfVehiclesOwn;
+    }
+
+
 }
