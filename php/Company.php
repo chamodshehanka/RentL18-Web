706c0dc6bd79c86fd3587e667b6217a0f67526e7
Shehanka <hcsperera@outlook.com>
2018-09-02 23:08:36 +0530

    Company class created

--- php/Company.php
+++ php/Company.php
@@ -8,5 +8,11 @@
 
 class Company
 {
+    private $companyID;
+    private $companyName;
+    private $addressLine1;
+    private $addressLine2;
+    private $contact;
+    private $noOfVehiclesOwn;
 
 }
