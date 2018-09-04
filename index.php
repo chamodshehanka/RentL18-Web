8be8fcff23a36c3da045bc60778ed2da796cde98
Shehanka <hcsperera@outlook.com>
2018-09-04 17:04:35 +0530

    Connection class working

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Date: 9/3/2018
  * Time: 10:47 PM
  */
-require_once ("controller/Connection.php");
+require_once "controller/Connection.php";
 
 $db = Connection::getInstance();
 $conn = $db->getConnection();
