577b98d1de768f68562b56f865770405f72763cb
Shehanka <hcsperera@outlook.com>
2018-09-06 22:58:41 +0530

    User dialog making

--- controller/setUserDetails.php
+++ controller/setUserDetails.php
@@ -11,11 +11,13 @@ if(isset($_COOKIE[$cookie_name])) {
 //    echo "Cookie '" . $cookie_name . "' is set!<br>";
 //    echo "Value is: " . $_COOKIE[$cookie_name];
     $userName = $_COOKIE[$cookie_name];
+    echo '<h1>User</h1>'
     ?>
     <script type="text/javascript">
         let userName = "<?php echo $userName; ?>";
         console.log(userName);
-        $('#navUserName').val(userName)
+        $('#navUserName').val('hello '+userName)
+
     </script>
     <?php
 } else {
