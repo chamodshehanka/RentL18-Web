9935e62f73967255cb73ebb1e8314f63f43c9543
Shehanka <hcsperera@outlook.com>
2018-09-26 18:52:41 +0530

    settings making

---
+++ user-profile.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chamodshehanka
+ * Date: 9/26/2018
+ * Time: 6:50 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile | Admin</title>
+
+    <?php include 'header-tags.php'?>
+</head>
+<body>
+
+<?php include 'header.php'?>
+
+<?php include 'side-menu.php'?>
+
+<!--Body section-->
+<div style="width: 100%;height: 500px"></div>
+<!--End Body section-->
+
+<?php include 'footer.php'?>
+<?php include 'controller/setUserDetails.php';?>
+</body>
+</html>
+
+
