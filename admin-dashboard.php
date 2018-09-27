7904755c3a12623e420b15dd426b32e45cc54450
Shehanka <hcsperera@outlook.com>
2018-09-28 02:10:50 +0530

    manage admin making

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -20,15 +20,22 @@
 <?php include 'header.php'?>
 
 <?php include 'side-menu.php'?>
-<div style="width: 1px;height: 50px"></div>
+
+<div style="width: 100%; height: 30px;"></div>
 <!--Body section-->
 <div class="container">
-    <div class="uk-card uk-card-primary uk-card-body uk-width-1-2@m"
-         style="width: 200px;height: 200px;">
-        <h3 class="uk-card-title" align="center">Users</h3>
-        <img src="https://png.icons8.com/color/96/3498db/conference.png">
+    <div class="uk-animation-toggle">
+        <div class="uk-card uk-card-primary uk-card-body uk-animation-fade uk-width-1-2@m"
+             style="width: 200px;height: 200px;">
+            <h3 class="uk-card-title" align="center">Users</h3>
+            <p align="center">
+                <img src="https://png.icons8.com/color/96/3498db/conference.png">
+            </p>
+        </div>
     </div>
 </div>
+
+<div style="width: 100%; height: 30px"></div>
 <!--End Body section-->
 
 <?php include 'footer.php'?>
