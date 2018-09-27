ea8079b2743f4c0b5815b0ee108d268299b36af8
Shehanka <hcsperera@outlook.com>
2018-09-28 00:08:05 +0530

    settings making

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -22,8 +22,8 @@
 <?php include 'side-menu.php'?>
 
 <!--Body section-->
-<div style="">
-    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+<div class="container">
+    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m" style="width: 300px">
         <h3 class="uk-card-title">Default</h3>
         <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
