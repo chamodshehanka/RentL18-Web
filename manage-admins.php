165c6adb78a44680c494a427bc125b204c883fb5
Shehanka <hcsperera@outlook.com>
2018-09-28 01:41:58 +0530

    manage  admin making

--- manage-admins.php
+++ manage-admins.php
@@ -5,3 +5,27 @@
  * Date: 9/28/2018
  * Time: 1:35 AM
  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manage | Admin</title>
+
+    <?php include 'header-tags.php'?>
+</head>
+<body>
+<?php include 'header.php'?>
+<?php include 'side-menu.php'?>
+
+<!--Body-->
+
+
+
+<!--End Body-->
+
+<?php include 'footer.php'?>
+<?php include 'controller/setUserDetails.php';?>
+</body>
+</html>
