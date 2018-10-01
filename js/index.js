18ad773b96984d223a8938e784684de4a10bc46f
Shehanka <hcsperera@outlook.com>
2018-10-01 22:48:52 +0530

    comments were added

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     btnSignIn.click(function () {
         let email = $('#txtEmail').val();
         let password = $('#txtPassword').val();
-        console.log(email + password);
+
         if (!(email === "" || password === "")){
             $.ajax({
                 url : 'controller/signIn.php',
