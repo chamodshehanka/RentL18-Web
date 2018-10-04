5ff9847256a7b31fda7f742d4d2252012f5d3e0a
Shehanka <hcsperera@outlook.com>
2018-10-04 16:09:37 +0530

    AJAX worked

--- js/ajax-scripts.js
+++ js/ajax-scripts.js
@@ -20,35 +20,3 @@ $("#btnAddAdmin").click(function(){
 
     http.send(queryString);
 });
-
-
-
-
-
-
-// let btnAddAdmin = $('#btnAddAdmin');
-/*
-btnAddAdmin.click(function () {
-    let data = $('#form-add-admin:input').serializeArray();
-    $.post($('#form-add-admin').attr("action"), data, function (info) {
-        if (info === 'Added'){
-            console.log(info);
-            UIkit.notification({message: 'Admin has been successfully added!'});
-        }else {
-            UIkit.notification({message: 'Failed to add admin!'});
-        }
-    });
-});
-
-btnAddAdmin.submit(function () {
-    return false;
-});*/
-
-/*btnAddAdmin.click(function () {
-    let name = $('name');
-
-    $.ajax({
-        type : 'POST',
-        // data : ();
-    })
-});*/
